98f2e213d1746e5b9c0d4889d7d1fc00f75fe81d
thirumurugan ganesan <thirutesting@gmail.com>
2021-08-22 23:04:29 +0530

    Second Commit

--- src/com/verizon/test/FirstJavaClass.java
+++ src/com/verizon/test/FirstJavaClass.java
@@ -5,5 +5,6 @@ public class FirstJavaClass {
 	public static void main(String[] args) {
 		System.out.println("Hi Thirumurugan... Welcome To The Java Project");
 		System.out.println("And We are welcoming you to the Git World");
+		System.out.println("And We are welcoming you to the Git World");
 	}
 }
