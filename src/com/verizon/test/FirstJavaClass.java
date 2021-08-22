c95b41263b3b62edc6ba0e35535acab5cdab545d
thirumurugan ganesan <thirutesting@gmail.com>
2021-08-22 22:22:57 +0530

    First Code Commit for DemoRepo

---
+++ src/com/verizon/test/FirstJavaClass.java
@@ -0,0 +1,8 @@
+package com.verizon.test;
+
+
+public class FirstJavaClass {
+	public static void main(String[] args) {
+		System.out.println("Hi Thirumurugan... Welcome To The Java Project");
+	}
+}
